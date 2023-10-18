723da368b390e16ce550d8bf490463916a6fac5d
deevil <deevilx@gmail.com>
2023-10-18 19:12:20 +0300

    inc log objects

---
+++ OneSTools.EventLog.Exporter.Core/Log/ILoggerBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OneSTools.EventLog.Exporter.Core.Log
+{
+    public static class LoggerBuilderExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder loggingBuilder, string path)
+        {
+            var provider = new FileLoggerProvider(path);
+
+            return loggingBuilder.AddProvider(provider);
+        }
+    }
+}
